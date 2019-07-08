c1d164ea67192edc3258bc6f71b2bc938825beb7
Divvela Mounika <mdivvela@virtusa.com>
2019-07-08 12:54:13 +0530

    MetaDataService

---
+++ resolvers.js
@@ -0,0 +1,23 @@
+const db = require('./db')
+const Query = {
+  
+  
+   students:() => db.students.list(),
+
+ 
+   studentById:(root,args,context,info) => {
+
+      return db.students.get(args.id);
+   }
+}
+
+const Student = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+":"+root.lastName
+   },
+   college:(root) => {
+      return db.colleges.get(root.collegeId);
+   }
+}
+
+module.exports = {Query,Student}
