22f7db6ef041c6a9676068fca035f6ea5bcefa7a
Divvela Mounika <mdivvela@virtusa.com>
2019-07-26 12:29:00 +0530

    LoaderExample

---
+++ src/app/loader.model.ts
@@ -0,0 +1,18 @@
+interface Loader {
+    size: string;
+
+  }
+  
+ export const Loaders: Loader[] = [
+     {
+         size:"small"
+     },
+     {
+         size:"medium"
+     },
+     {
+         size:"large"
+     }
+
+
+  ];
