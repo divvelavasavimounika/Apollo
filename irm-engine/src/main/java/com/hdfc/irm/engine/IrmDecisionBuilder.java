74f6793885602ebabcb7bfc6630bdbae220ad283
Divvela Mounika <mdivvela@virtusa.com>
2020-01-09 09:51:15 +0530

    Initial commit

---
+++ irm-engine/src/main/java/com/hdfc/irm/engine/IrmDecisionBuilder.java
@@ -0,0 +1,8 @@
+package com.hdfc.irm.engine;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IrmDecisionBuilder {
+
+}
---
+++ irm-web/src/test/java/com/hdfc/irm/web/IrmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfc.irm.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrmWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
